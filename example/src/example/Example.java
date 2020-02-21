593e6c6e0aa8ad696c1f4290556021941f3368bd
oduwolem <oduwolem@sheridancollege.ca>
2020-02-21 08:43:46 -0500

    comitted by me on 06/02/2020

---
+++ example/src/example/Example.java
@@ -0,0 +1,21 @@
+/*
+ * Moses Oduwole
+ *  991553625
+ *  
+ */
+package example;
+
+/**
+ *
+ * @author class
+ */
+public class Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
